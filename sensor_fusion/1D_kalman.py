d4f567703377be6a8b6ebe51ddfd5706c974df58
yao Liu <liuyao199111@163.com>
2023-01-23 22:50:56 +0800

    Update 1D_kalman.py
    add comments for 1_d kalman

--- sensor_fusion/1D_kalman.py
+++ sensor_fusion/1D_kalman.py
@@ -1,6 +1,6 @@
 def update(mean1, var1, mean2, var2):
-    new_mean = float(var2 * mean1 + var1 * mean2) / (var1 + var2)
-    new_var = 1./(1./var1 + 1./var2)
+    new_mean = float(var2 * mean1 + var1 * mean2) / (var1 + var2)  # 新均值位于mean1和mean2之间
+    new_var = 1./(1./var1 + 1./var2)  # 新方差要比var1和var2均小，高斯曲线更加高耸，陡峭
     return [new_mean, new_var]
 
 def predict(mean1, var1, mean2, var2):
